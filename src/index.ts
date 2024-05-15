d66baf29acd0f8c73f68c46ef85b8b0a2feb425d
SajalVaishnav <76965917+SajalVaishnav@users.noreply.github.com>
2024-05-15 22:22:58 +0530

    latest db schema

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,8 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 import { createClient } from '@supabase/supabase-js';
 import { Tables } from '@/database.types';
 import { getMeterCreditFromMeteridPassword } from '@/evs-crawler';
 
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
 	async scheduled(event: ScheduledEvent, env: Env) {
 		if(!env.SUPABASE_URL || !env.SUPABASE_KEY || env.SUPABASE_URL === '' || env.SUPABASE_KEY === ''	) {
 			throw new Error('Missing SUPABASE_URL or SUPABASE_KEY');
@@ -39,10 +21,10 @@ export default {
 		} = await supabase
 			.from('Meter')
 			.select()
-			.gte('readingUpdatedAt', new Date(Date.now() - 86400000).toISOString()) // 24 hours ago
+			.lte('readingUpdatedAt', new Date(Date.now() - 86400000).toISOString()) // 24 hours ago
 			.limit(100);
 
-		if (fetchMetersError || status !== 200 || !meters) {
+		if (fetchMetersError || status !== 200 || !meters || meters.length === 0) {
 			console.error('Failed fetching meters or no meters found: ', fetchMetersError, statusText);
 			return 
 		}
@@ -61,16 +43,26 @@ export default {
 				}
 
 				console.log(`Successfully created MeterReading for meterId ${meterId}`);
+
+				const { status: updateStatus, statusText: updateStatusText } = await supabase
+					.from('Meter')
+					.update({ readingUpdatedAt: new Date().toISOString() })
+					.match({ meterId });
+				if(updateStatus !== 204) {
+					throw new Error(`Failed updating Meter readingUpdatedAt for meterId ${meterId}: ${updateStatusText}`);
+				}
+				
+				console.log(`Successfully completed for meterId ${meterId}`);
 			} catch (error) {
-				console.error(`Failed creating MeterReading for meterId ${meter.meterId}:`, error);
+				console.error(`Failed for meterId ${meter.meterId}:`, error);
 			}
 		});
 
 		await Promise.all(promises);
 
 		const endTime = new Date().toISOString();
-		console.log('Finished create-new-meter-readings-worker at', endTime);
 		const timeTaken = new Date(endTime).getTime() - new Date(startTime).getTime();
 		const message = `Finished create-new-meter-readings-worker in ${timeTaken} ms`;
+		console.log(message);
 	},
 };
